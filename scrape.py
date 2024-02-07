a2021dc018e778ec517ef494d883313c26277dfd
Lucas Joannes <joannesribeiro_@outlook.com>
2024-02-06 21:19:37 -0300

    implementado parser para coleta dos preços do livros

--- scrape.py
+++ scrape.py
@@ -10,7 +10,7 @@ class ScrapeBooks:
     def requests_page(self):
         try:
             response = requests.get(f"{self.url_base}index.html")
-            response.raise_for_status()  # Raise an HTTPError for bad responses
+            response.raise_for_status()
             self.soup = BeautifulSoup(response.text, "html.parser")
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}")
@@ -46,11 +46,26 @@ class ScrapeBooks:
                         print(title_book.text)
             except requests.exceptions.RequestException as e:
                 print(f"Error fetching {url_thumbnail}: {e}")
+                
+    def parser_price_book(self):
+        for url_thumbnail in self.thumbnail_addresses:
+            try:
+                response_catalogue = requests.get(f"{self.url_base}{url_thumbnail}")
+                response_catalogue.raise_for_status()
+                soup_book = BeautifulSoup(response_catalogue.text, "html.parser")
+                for informations in soup_book.find_all(
+                    "table", class_="table table-striped"
+                ):
+                    for price_book in informations.find_all("td")[3]:
+                        print(price_book.text)
+            except requests.exceptions.RequestException as e:
+                print(f"Error fetching {url_thumbnail}: {e}")
 
-
+                
 # Example usage:
 scraper = ScrapeBooks()
 scraper.requests_page()
 scraper.parser_thumbnail_address()
 scraper.get_thumbnail_address()
 scraper.parser_title_book()
+scraper.parser_price_book()
